b19bd964bff7427cb61be968aff8808649e21230
bowmans.dev <bowmans.dev@gmail.com>
2023-02-03 21:53:04 +0000

    mobile_testing_v0

---
+++ scroll.js
@@ -0,0 +1,60 @@
+function pageScroll() {
+  window.scrollBy(0, 2); // x and y scroll values
+  scrolldelay = setTimeout('pageScroll()', 65); // scrolls every 65 milliseconds
+}
+
+function stopScroll() {
+  clearTimeout(scrolldelay);
+  window.scrollBy(0, 0);
+  if (window.scrollY > 0) {
+    scrolldelay = setTimeout('stopScroll()', 0);
+  } else {
+    clearTimeout(scrolldelay);
+  }   
+}
+
+function keyboardStopPageScroll() {
+
+  clearTimeout(scrolldelay);
+  window.scrollBy(0, 0);
+  if (window.scrollY > 0) {
+    scrolldelay = setTimeout('stopScroll()', 0);
+  } else {
+    clearTimeout(scrolldelay);
+  } 
+}
+function topFunction() {
+  
+  document.documentElement.scrollTop = 0;
+  keyboardStopPageScroll();
+} 
+
+//BACK TO TOP KEYBOARD SHORTCUTS
+// on UP⬆︎ key keypress, scroll up to the top of the page
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 38) {
+    topFunction();
+    keyboardStopPageScroll();
+  }
+});
+// on LEFT ⬅︎ key keypress, stop scrolling COMPLETELY ////////
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 37) {
+    stopScroll();
+    keyboardStopPageScroll();
+  }
+});
+
+// SCROLL DOWN KEYBOARD SHORTCUTS
+// on DOWN ⬇︎ key keypress, scroll down or increase scroll speed 
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 40) {
+    pageScroll();
+  }
+});
+// on RIGHT ➡︎ key keypress, scroll down or increase speed
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 39) {
+    pageScroll();
+  }
+});
